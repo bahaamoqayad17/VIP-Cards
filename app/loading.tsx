cad0713ca2bc9506a58949900642858b001254a3
bahaamoqayad17 <bahaa200030.m7taref@gmail.com>
2026-01-24 20:54:55 +0200

    fix(loading): remove erroneous semicolon in JSX

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@
 export default function Loading() {
     return (
         <div className="flex min-h-screen justify-center items-center">
-            <div className="custom-loader"></div>;
+            <div className="custom-loader"></div>
         </div>
     )
 }
